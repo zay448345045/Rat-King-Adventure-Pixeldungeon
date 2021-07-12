c3747a2ac86303d1b754946314ecae1c3c411b05
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-07-12 23:15:02 +0300

    added 4th clone talent

--- core/src/main/java/com/zrp200/rkpd2/actors/hero/abilities/rogue/ShadowClone.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/hero/abilities/rogue/ShadowClone.java
@@ -21,6 +21,14 @@
 
 package com.zrp200.rkpd2.actors.hero.abilities.rogue;
 
+import com.watabou.noosa.TextureFilm;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.noosa.particles.Emitter;
+import com.watabou.noosa.tweeners.Tweener;
+import com.watabou.utils.Bundle;
+import com.watabou.utils.Callback;
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.Random;
 import com.zrp200.rkpd2.Assets;
 import com.zrp200.rkpd2.Dungeon;
 import com.zrp200.rkpd2.actors.Actor;
@@ -30,22 +38,20 @@ import com.zrp200.rkpd2.actors.hero.Hero;
 import com.zrp200.rkpd2.actors.hero.Talent;
 import com.zrp200.rkpd2.actors.hero.abilities.ArmorAbility;
 import com.zrp200.rkpd2.actors.mobs.npcs.DirectableAlly;
+import com.zrp200.rkpd2.effects.CellEmitter;
+import com.zrp200.rkpd2.effects.Speck;
 import com.zrp200.rkpd2.effects.particles.SmokeParticle;
 import com.zrp200.rkpd2.items.armor.ClassArmor;
+import com.zrp200.rkpd2.items.weapon.missiles.Kunai;
 import com.zrp200.rkpd2.levels.CityLevel;
+import com.zrp200.rkpd2.mechanics.Ballistica;
 import com.zrp200.rkpd2.messages.Messages;
 import com.zrp200.rkpd2.scenes.GameScene;
 import com.zrp200.rkpd2.sprites.HeroSprite;
+import com.zrp200.rkpd2.sprites.MissileSprite;
 import com.zrp200.rkpd2.sprites.MobSprite;
 import com.zrp200.rkpd2.utils.BArray;
 import com.zrp200.rkpd2.utils.GLog;
-import com.watabou.noosa.TextureFilm;
-import com.watabou.noosa.audio.Sample;
-import com.watabou.noosa.particles.Emitter;
-import com.watabou.noosa.tweeners.Tweener;
-import com.watabou.utils.Bundle;
-import com.watabou.utils.PathFinder;
-import com.watabou.utils.Random;
 
 import java.util.ArrayList;
 
@@ -119,7 +125,7 @@ public class ShadowClone extends ArmorAbility {
 
 	@Override
 	public Talent[] talents() {
-		return new Talent[]{Talent.SHADOW_BLADE, Talent.CLONED_ARMOR, Talent.PERFECT_COPY, Talent.HEROIC_ENERGY};
+		return new Talent[]{Talent.SHADOW_BLADE, Talent.CLONED_ARMOR, Talent.PERFECT_COPY, Talent.DAR_MAGIC, Talent.HEROIC_ENERGY};
 	}
 
 	private static ShadowAlly getShadowAlly(){
@@ -152,6 +158,85 @@ public class ShadowClone extends ArmorAbility {
 				HP += hpBonus;
 			}
 			defenseSkill = heroLevel + 5; //equal to base hero defense skill
+		}
+
+		@Override
+		public boolean canAttack(Char enemy) {
+			if (Dungeon.hero.pointsInTalent(Talent.DAR_MAGIC) > 0){
+				Ballistica attack = new Ballistica( pos, enemy.pos, Ballistica.PROJECTILE);
+				return attack.collisionPos == enemy.pos;
+			}
+			return super.canAttack(enemy);
+		}
+
+		protected boolean doAttack( Char enemy ) {
+			if (Dungeon.level.adjacent(pos, enemy.pos)){
+				return super.doAttack( enemy );
+			} else {
+
+				if (sprite != null && (sprite.visible || enemy.sprite.visible)) {
+					if (Dungeon.hero.pointsInTalent(Talent.DAR_MAGIC) < 3) {
+						((MissileSprite) sprite.parent.recycle(MissileSprite.class)).
+								reset(sprite,
+										enemy.sprite,
+										new Kunai(),
+										new Callback() {
+											@Override
+											public void call() {
+												attack(enemy,
+														0.25f * Dungeon.hero.pointsInTalent(Talent.DAR_MAGIC),
+														0, 1);
+												spend(attackDelay());
+												next();
+											}
+										});
+					} else {
+						//teleport
+						int bestPos = -1;
+						for (int i = 0; i < PathFinder.NEIGHBOURS8.length; i++) {
+							int p = enemy.pos + PathFinder.NEIGHBOURS8[i];
+							if (Actor.findChar( p ) == null && Dungeon.level.passable[p]) {
+								bestPos = p;
+							}
+						}
+						if (bestPos != -1){
+							appear(this, bestPos);
+							sprite.attack(enemy.pos, new Callback() {
+								@Override
+								public void call() {
+									attack(enemy);
+									spend(attackDelay());
+
+											if (Dungeon.hero.pointsInTalent(Talent.DAR_MAGIC) == 4){
+												int count = 32;
+												int newPos = -1;
+												for (int i : PathFinder.NEIGHBOURS8){
+													if (Actor.findChar( Dungeon.hero.pos + i ) == null && Dungeon.level.passable[Dungeon.hero.pos + i]){
+														newPos = Dungeon.hero.pos + i;
+													}
+												}
+
+												if (newPos != -1) {
+
+													if (Dungeon.level.heroFOV[pos]) CellEmitter.get(pos).burst(Speck.factory(Speck.WOOL), 6);
+													appear(ShadowAlly.this, newPos);
+													if (Dungeon.level.heroFOV[pos]) CellEmitter.get(pos).burst(Speck.factory(Speck.WOOL), 6);
+												}
+
+
+											}
+									next();
+								}
+							});
+							return false;
+//							spend(attackDelay());
+						}
+					}
+					return false;
+				} else {
+					return super.doAttack(enemy);
+				}
+			}
 		}
 
 		@Override
@@ -190,6 +275,9 @@ public class ShadowClone extends ArmorAbility {
 			heroDamage = Math.round(0.0625f * Dungeon.hero.shiftedPoints(Talent.SHADOW_BLADE) * heroDamage);
 			if (heroDamage > 0){
 				damage += heroDamage;
+			}
+			if (Dungeon.hero.pointsInTalent(Talent.DAR_MAGIC) == 3){
+				damage *= 0.75f;
 			}
 			return damage;
 		}
