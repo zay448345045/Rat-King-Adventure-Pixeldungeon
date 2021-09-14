c4c80e22bb8b5ab9b15e80b8d815d4463c3f3136
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2021-09-14 23:31:50 +0300

    removed debug stuff again

--- core/src/main/java/com/zrp200/rkpd2/actors/hero/HeroClass.java
+++ core/src/main/java/com/zrp200/rkpd2/actors/hero/HeroClass.java
@@ -41,7 +41,6 @@ import com.zrp200.rkpd2.actors.hero.abilities.warrior.HeroicLeap;
 import com.zrp200.rkpd2.actors.hero.abilities.warrior.Shockwave;
 import com.zrp200.rkpd2.items.BrokenSeal;
 import com.zrp200.rkpd2.items.Item;
-import com.zrp200.rkpd2.items.TengusMask;
 import com.zrp200.rkpd2.items.Waterskin;
 import com.zrp200.rkpd2.items.armor.ClothArmor;
 import com.zrp200.rkpd2.items.armor.ScoutArmor;
@@ -51,7 +50,10 @@ import com.zrp200.rkpd2.items.bags.PotionBandolier;
 import com.zrp200.rkpd2.items.bags.ScrollHolder;
 import com.zrp200.rkpd2.items.bags.VelvetPouch;
 import com.zrp200.rkpd2.items.food.Food;
-import com.zrp200.rkpd2.items.potions.*;
+import com.zrp200.rkpd2.items.potions.PotionOfHealing;
+import com.zrp200.rkpd2.items.potions.PotionOfInvisibility;
+import com.zrp200.rkpd2.items.potions.PotionOfLiquidFlame;
+import com.zrp200.rkpd2.items.potions.PotionOfMindVision;
 import com.zrp200.rkpd2.items.scrolls.*;
 import com.zrp200.rkpd2.items.wands.Wand;
 import com.zrp200.rkpd2.items.wands.WandOfMagicMissile;
@@ -129,10 +131,6 @@ public enum HeroClass {
 
 		Waterskin waterskin = new Waterskin();
 		waterskin.collect();
-		new TengusMask().collect();
-		for (int l = 0; l < 18; l++){
-			new PotionOfExperience().apply(hero);
-		}
 
 		new ScrollOfIdentify().identify();
 
