0c588b06017e9e4da1814c84af8c8c5510f91d91
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-01-17 04:54:48 +0300

    1.4.10: protein infusion no longer triggers rogue's foresight

--- core/src/main/java/com/zrp200/rkpd2/scenes/GameScene.java
+++ core/src/main/java/com/zrp200/rkpd2/scenes/GameScene.java
@@ -496,10 +496,8 @@ public class GameScene extends PixelScene {
 				}
 				//50%/75% chance for power within, use level's seed so that we get the same result for the same level
 				//60/90% chance for rogue's foresight
-				float chance = Dungeon.hero.byTalent(
-						false, false,
-						Talent.ROGUES_FORESIGHT,.30f,
-						Talent.POWER_WITHIN, .25f);
+				float chance = .25f + Dungeon.hero.pointsInTalent(
+						Talent.POWER_WITHIN) * .25f;
 				Random.pushGenerator(Dungeon.seedCurDepth());
 				if (reqSecrets <= 0 && Random.Float() <= chance) {
 					GLog.p(Messages.get(this, "secret_hint"));
