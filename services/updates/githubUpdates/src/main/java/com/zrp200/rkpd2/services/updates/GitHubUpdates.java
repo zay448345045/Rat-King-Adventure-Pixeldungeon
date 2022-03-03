cfa174d0b739ace29375f71d9b24e01a083bd74f
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-03-03 20:34:14 +0300

    1.4.14: fixed github updates

--- services/updates/githubUpdates/src/main/java/com/zrp200/rkpd2/services/updates/GitHubUpdates.java
+++ services/updates/githubUpdates/src/main/java/com/zrp200/rkpd2/services/updates/GitHubUpdates.java
@@ -56,7 +56,7 @@ public class GitHubUpdates implements UpdateService {
 		}
 
 		Net.HttpRequest httpGet = new Net.HttpRequest(Net.HttpMethods.GET);
-		httpGet.setUrl("https://api.github.com/repos/TrashboxBobylev/rkpd2/releases");
+		httpGet.setUrl("https://api.github.com/repos/TrashboxBobylev/Rat-King-Adventure/releases");
 		httpGet.setHeader("Accept", "application/vnd.github.v3+json");
 
 		Gdx.net.sendHttpRequest(httpGet, new Net.HttpResponseListener() {
