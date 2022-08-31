1ff734f51afa88f3ebf391478c77703473528550
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-09-01 02:39:14 +0300

    1.5: implemented most of the warp code
    most of the constants is controlled in WarpPile, making it the target point for all warp-related activities

---
+++ core/src/main/java/com/zrp200/rkpd2/actors/buffs/Warp.java
@@ -0,0 +1,114 @@
+package com.zrp200.rkpd2.actors.buffs;
+
+import com.watabou.utils.Bundle;
+import com.watabou.utils.Random;
+import com.zrp200.rkpd2.Dungeon;
+import com.zrp200.rkpd2.messages.Messages;
+import com.zrp200.rkpd2.ui.BuffIndicator;
+import com.zrp200.rkpd2.utils.WarpPile;
+
+public class Warp extends Buff {
+    {
+        revivePersists = true;
+    }
+
+    private float stacks = 0;
+    private float decay = 0;
+
+    @Override
+    public int icon() {
+        return BuffIndicator.WARP;
+    }
+
+    @Override
+    public String toString() {
+        return Messages.get(this, "name");
+    }
+
+    @Override
+    public String desc() {
+        return Messages.get(this, "desc", getStacks(), getDecay());
+    }
+
+    @Override
+    public String iconTextDisplay() {
+        return Integer.toString(Math.round(getStacks()) * Math.round(getDecay()));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean act() {
+        if (target.isAlive()) {
+
+            spend( decay );
+            if (Random.Int(WarpPile.EFFECT_CHANCE) == 0){
+                float[] category = WarpPile.getChanceCat(Math.round(getStacks()));
+                int categoryID = Random.chances(category);
+                if (Random.Int(100) < category[categoryID]){
+                    WarpPile.WarpEffect effect = (WarpPile.WarpEffect) Random.chances(WarpPile.effectTypes[categoryID]);
+                    effect.call();
+                }
+            }
+            if (--stacks <= 0) {
+                detach();
+            }
+
+        } else {
+
+            detach();
+
+        }
+        return true;
+    }
+
+    public static final String STACKS = "enemy_stacks";
+    public static final String DAMAGE = "damage_inc";
+
+    @Override
+    public void storeInBundle(Bundle bundle) {
+        super.storeInBundle(bundle);
+        bundle.put(STACKS, getStacks());
+        bundle.put(DAMAGE, getDecay());
+    }
+
+    @Override
+    public void restoreFromBundle(Bundle bundle) {
+        super.restoreFromBundle(bundle);
+        setStacks(bundle.getFloat(STACKS));
+        setDecay(bundle.getFloat(DAMAGE));
+    }
+
+    public static Warp inflict(float stacks, float decay){
+        Warp effect = Buff.affect(Dungeon.hero, Warp.class);
+        effect.setStacks(stacks);
+        effect.setDecay(decay);
+        effect.postpone(WarpPile.DECAY_DELAY*decay);
+        return effect;
+    }
+
+    public static Warp modify(float stacks){
+        Warp effect = Buff.affect(Dungeon.hero, Warp.class);
+        effect.setStacks(effect.stacks + stacks);
+        return effect;
+    }
+
+    public static float stacks(){
+        return Buff.affect(Dungeon.hero, Warp.class).stacks;
+    }
+
+    public float getStacks() {
+        return stacks;
+    }
+
+    public void setStacks(float stacks) {
+        this.stacks = Math.min(WarpPile.MAX_WARP, this.stacks + stacks);
+    }
+
+    public float getDecay() {
+        return decay;
+    }
+
+    public void setDecay(float decay) {
+        this.decay = decay;
+    }
+}
--- core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
+++ core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
@@ -1,5 +1,56 @@
 package com.zrp200.rkpd2.utils;
 
+import com.watabou.noosa.audio.Sample;
+import com.watabou.utils.Callback;
+import com.zrp200.rkpd2.Assets;
+import com.zrp200.rkpd2.actors.buffs.Warp;
+
+import java.util.HashMap;
+
 public class WarpPile {
+
     public static final int COLOR = 0xc22ec9;
+
+    //initial wait in turns before warp will start decaying
+    public static final float DECAY_DELAY = 8;
+    //the (1/X) portion of warp that will be used to use an effect
+    public static final float EFFECT_BASE = 5;
+    //the cap for how much warp you can have
+    public static final float MAX_WARP = 150;
+    //the (1/X) chance for effect to come out every time the warp decays
+    public static final int EFFECT_CHANCE = 10;
+
+    public interface WarpEffect extends Callback {
+        void doEffect();
+
+        @Override
+        default void call(){
+            Warp.modify(Warp.stacks()/EFFECT_BASE);
+            Sample.INSTANCE.play(Assets.Sounds.CURSED);
+            doEffect();
+        }
+    }
+
+    public static float[][] categoryChances = {
+            {90, 60, 20},
+            {8, 30, 45},
+            {2, 10, 35}
+    };
+
+    public static float[] getChanceCat(int warp){
+        if (warp >= COMMON_THRESHOLD && warp < UNCOMMON_THRESHOLD) return categoryChances[0];
+        else if (warp >= UNCOMMON_THRESHOLD && warp < RARE_THRESHOLD) return categoryChances[1];
+        else return categoryChances[2];
+    }
+
+    public static int COMMON_THRESHOLD = 10;
+    public static HashMap<WarpEffect, Float> commonEffects = new HashMap<>();
+
+    public static int UNCOMMON_THRESHOLD = 50;
+    public static HashMap<WarpEffect, Float> uncommonEffects = new HashMap<>();
+
+    public static int RARE_THRESHOLD = 100;
+    public static HashMap<WarpEffect, Float> rareEffects = new HashMap<>();
+
+    public static HashMap[] effectTypes = new HashMap[]{commonEffects, uncommonEffects, rareEffects};
 }
