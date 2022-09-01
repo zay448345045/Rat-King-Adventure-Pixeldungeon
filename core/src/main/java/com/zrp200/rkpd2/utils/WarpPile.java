a898a510664e17c94ff3f95df3876020910363a0
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-09-01 17:53:55 +0300

    1.5: increased the frequency of warp effects

--- core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
+++ core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
@@ -22,13 +22,13 @@ public class WarpPile {
     //initial wait in turns before warp will start decaying
     public static final float DECAY_DELAY = 4;
     //the (1/X) portion of warp that will be used to use an effect
-    public static final float EFFECT_BASE = 6;
+    public static final float EFFECT_BASE = 8;
     //the cap for how much warp you can have
     public static final float MAX_WARP = 150;
 
     //the (1/X) chance for effect to come out every time the warp decays
     public static int effectChance(int warpAmount){
-        return 7 - warpAmount / 50;
+        return 8 - warpAmount / 30;
     }
 
     public interface WarpEffect extends Callback {
