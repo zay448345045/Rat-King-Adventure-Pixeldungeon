286e2a40d4e8eb715f993a804aa3de5986b4bcac
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2022-09-08 17:14:10 +0300

    1.5: reduced the frequency of warp effects

--- core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
+++ core/src/main/java/com/zrp200/rkpd2/utils/WarpPile.java
@@ -45,7 +45,7 @@ public class WarpPile {
 
     //the (1/X) chance for effect to come out every time the warp decays
     public static int effectTimer(float warpAmount){
-        return (int) (20 - warpAmount / 10f);
+        return (int) (30 - warpAmount / 10f);
     }
 
     public interface WarpEffect extends Callback {
